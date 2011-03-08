20cb094ef18a510bb948516fb98937962eb0a761
Sandro <sandropadin@gmail.com>
2011-03-08 16:31:41 -0600

    Remove HTTP Authentication.

--- q_r_code_this_app.rb
+++ q_r_code_this_app.rb
@@ -3,14 +3,6 @@ require 'sinatra'
 
 class QRCodeThisApp < Sinatra::Base
   set :public, File.dirname(__FILE__) + '/public'
-  
-  # Basic Authentication
-  # Asks for password only if on Heroku
-  if ENV['RACK_ENV'] == 'production' then
-    use Rack::Auth::Basic, "Restricted Area" do |username, password|
-      [username, password] == ['comdex', 'comdexpass1']
-    end
-  end
 
   get '/' do 
     redirect '/index.html' 
